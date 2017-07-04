6a2435becd4b60484f8f9f26f042b09c516df75e
k8tems <k8tems@live.com>
2017-07-05 08:49:44 +0900

    Change import

--- test.py
+++ test.py
@@ -1,40 +1,40 @@
 import unittest
-import save
+import process
 
 
 class TestRemoveUrl(unittest.TestCase):
     def test(self):
         expected = 'foo foo'
-        self.assertEqual(expected, save.remove_url('foo https://t.co/bar foo'))
+        self.assertEqual(expected, process.remove_url('foo https://t.co/bar foo'))
 
     def test_eol(self):
         expected = 'foo '
-        self.assertEqual(expected, save.remove_url('foo https://t.co/bar'))
+        self.assertEqual(expected, process.remove_url('foo https://t.co/bar'))
 
     def test_greed(self):
         expected = 'foo '
         # `foo` will also be replaced if the regex engine is greedy
-        self.assertEqual(expected, save.remove_url('https://t.co/bar foo '))
+        self.assertEqual(expected, process.remove_url('https://t.co/bar foo '))
 
 
 class TestRemoveMention(unittest.TestCase):
     def test(self):
         expected = 'foo foo'
-        self.assertEqual(expected, save.remove_mention('foo @bar foo'))
+        self.assertEqual(expected, process.remove_mention('foo @bar foo'))
 
     def test_eol(self):
         expected = 'foo '
-        self.assertEqual(expected, save.remove_mention('foo @bar'))
+        self.assertEqual(expected, process.remove_mention('foo @bar'))
 
     def test_greed(self):
         expected = 'foo '
-        self.assertEqual(expected, save.remove_mention('@bar foo '))
+        self.assertEqual(expected, process.remove_mention('@bar foo '))
 
 
 class TestRemoveRtBoilerplate(unittest.TestCase):
     def test(self):
         expected = 'foo'
-        self.assertEqual(expected, save.remove_rt_boilerplate('RT @bar: foo'))
+        self.assertEqual(expected, process.remove_rt_boilerplate('RT @bar: foo'))
 
 
 if __name__ == '__main__':
