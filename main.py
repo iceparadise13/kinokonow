aeea7d08b6263cf1d91906f1359389b99cca4778
k8tems <k8tems@live.com>
2017-07-04 18:09:06 +0900

    Stream tweets

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+import yaml
+from twython import TwythonStreamer
+
+
+class Streamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            print data['text'].encode('utf-8')
+
+    def on_error(self, status_code, data):
+        print status_code
+        self.disconnect()
+
+
+def get_streamer():
+    cfg = yaml.load(open('twitter.yml', 'rb'))
+    return Streamer(
+        cfg['consumer_key'],
+        cfg['consumer_secret'],
+        cfg['access_token_key'],
+        cfg['access_token_secret'])
+
+
 if __name__ == '__main__':
-    pass
+    stream = get_streamer()
+    stream.statuses.filter()
