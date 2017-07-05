b0ca44d094fa5e380a32c81df6dcd6f228a95a3f
k8tems <k8tems@live.com>
2017-07-05 16:18:26 +0900

    Print sorted frequencies

--- draw.py
+++ draw.py
@@ -1,5 +1,11 @@
+import pprint
+from operator import itemgetter
 import wordcloud
 from util import iterate_json
+
+
+def print_frequencies(frequencies):
+    pprint.pprint(sorted(list(frequencies.items()), key=itemgetter(1)))
 
 
 if __name__ == '__main__':
@@ -16,6 +22,8 @@ if __name__ == '__main__':
                 if n not in frequencies:
                     frequencies[n] = 0
                 frequencies[n] += 1
+
+    print_frequencies(frequencies)
 
     cloud = wordcloud.WordCloud(font_path='font.ttf').generate_from_frequencies(frequencies)
     img = cloud.to_image()
