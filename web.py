20d699218aca119febab2d3d231ec8ee36dcf6f0
k8tems <k8tems@live.com>
2017-07-07 19:23:47 +0900

    Render frequencies

--- web.py
+++ web.py
@@ -1,5 +1,7 @@
 import pymongo
+from datetime import datetime, timedelta
 from flask import Flask, render_template
+import words
 
 
 app = Flask(__name__, template_folder='templates')
@@ -9,8 +11,11 @@ db = client.get_database('kinokonow')
 
 @app.route('/')
 def home():
-    return render_template('index.html', frequencies=[])
+    frequencies = words.get_filtered_noun_frequencies(
+        db.nouns, datetime.utcnow() - timedelta(hours=1), words.read_black_list())
+    frequencies = [[k, v] for k, v in frequencies.items()]
+    return render_template('index.html', frequencies=frequencies)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
