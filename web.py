30d560a433f11d395cb1ec3b54f16c88bfdaec7f
k8tems <k8tems@live.com>
2017-07-08 09:11:37 +0900

    Move celery related code to dedicated module

--- web.py
+++ web.py
@@ -1,40 +1,13 @@
-import pymongo
 from datetime import datetime, timedelta
-from flask import Flask, render_template
 from operator import itemgetter
+import pymongo
+from flask import Flask, render_template
 import words
-from celery import Celery
 
 
 flask_app = Flask(__name__, template_folder='templates')
-flask_app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379',
-    CELERY_RESULT_BACKEND='redis://localhost:6379'
-)
 client = pymongo.MongoClient(host='localhost', port=27017)
 db = client.get_database('kinokonow')
-
-
-def make_celery(app):
-    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
-                    broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
-
-
-celery = make_celery(flask_app)
-
-
-@celery.task()
-def add_together(a, b):
-    return a + b
 
 
 @flask_app.route('/')
