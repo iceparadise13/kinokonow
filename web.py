2438ff8dd47a51bf682642e71f75a4eb08e6cac1
k8tems <k8tems@live.com>
2017-07-08 09:07:45 +0900

    Add celery boilerplate

--- web.py
+++ web.py
@@ -3,14 +3,41 @@ from datetime import datetime, timedelta
 from flask import Flask, render_template
 from operator import itemgetter
 import words
+from celery import Celery
 
 
-app = Flask(__name__, template_folder='templates')
+flask_app = Flask(__name__, template_folder='templates')
+flask_app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
 client = pymongo.MongoClient(host='localhost', port=27017)
 db = client.get_database('kinokonow')
 
 
-@app.route('/')
+def make_celery(app):
+    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+
+celery = make_celery(flask_app)
+
+
+@celery.task()
+def add_together(a, b):
+    return a + b
+
+
+@flask_app.route('/')
 def home():
     frequencies = words.get_filtered_noun_frequencies(
         db.nouns, datetime.utcnow() - timedelta(hours=1), words.read_black_list())
@@ -20,4 +47,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    flask_app.run(debug=True, host='0.0.0.0')
