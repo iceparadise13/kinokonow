822c0760626e1a09f873232f2b4c2255416baca3
k8tems <k8tems@live.com>
2017-07-10 17:12:39 +0900

    Add --no-cache-dir to avoid memory issues

--- fabfile.py
+++ fabfile.py
@@ -14,4 +14,4 @@ def copy_files(fab_files, remote_path):
 def sync_requirements(requirements_file, remote_path):
     remote_requirements_file = os.path.join(remote_path, requirements_file)
     put(requirements_file, remote_requirements_file)
-    run('pip install -r ' + remote_requirements_file)
+    run('pip install --no-cache-dir -r ' + remote_requirements_file)
