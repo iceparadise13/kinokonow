d3212c1bc1e8a81a377808906bec7109fad96140
k8tems <k8tems@live.com>
2017-07-10 16:58:45 +0900

    Change var names

--- fabfile.py
+++ fabfile.py
@@ -6,5 +6,6 @@ def copy_files(fab_files, remote_path):
     files_to_deploy = open(fab_files).read().split()
     print('deploying', files_to_deploy)
     run('mkdir -p %s' % remote_path)
-    for f in files_to_deploy:
-        put(f, os.path.join(remote_path, f))
+    for src in files_to_deploy:
+        dest = os.path.join(remote_path, src)
+        put(src, dest)
