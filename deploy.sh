0500f33059e4ba4d909d6f43734d63dcc99082fc
k8tems <k8tems@live.com>
2017-07-10 16:53:33 +0900

    Deploy files in .fabfiles

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
-fab -H pi@192.168.0.14 copy_files:/home/k8tems/PycharmProjects/kinokonow,/home/pi/kinokonow
+DEST_DIR=/home/pi/kinokonow
+while read line; do
+  echo deploying $line
+  fab -H pi@192.168.0.14 copy_files:$line,${DEST_DIR}/$line
+done < .fabfiles
