91fa878f1c32cd680dbd6eb41c682c9ad4f14adf
k8tems <k8tems@live.com>
2017-07-10 17:00:06 +0900

    mkdir before each file

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ from fabric.api import run, put
 def copy_files(fab_files, remote_path):
     files_to_deploy = open(fab_files).read().split()
     print('deploying', files_to_deploy)
-    run('mkdir -p %s' % remote_path)
     for src in files_to_deploy:
         dest = os.path.join(remote_path, src)
+        run('mkdir -p %s' % os.path.dirname(dest))
         put(src, dest)
