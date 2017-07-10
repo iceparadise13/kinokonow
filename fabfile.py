ab44e07d52b19665ec3828a02b51d1234ef2e48a
k8tems <k8tems@live.com>
2017-07-10 17:28:20 +0900

    pip3 doesn't seem to support --no-cache-dirs

--- fabfile.py
+++ fabfile.py
@@ -14,4 +14,4 @@ def copy_files(fab_files, remote_path):
 def sync_requirements(requirements_file, remote_path):
     remote_requirements_file = os.path.join(remote_path, requirements_file)
     put(requirements_file, remote_requirements_file)
-    run('pip3 install --no-cache-dir -r ' + remote_requirements_file)
+    run('pip3 install -r ' + remote_requirements_file)
