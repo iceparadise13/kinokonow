11274a2e4758326c796c639fe13b75a890307c8d
k8tems <k8tems@live.com>
2017-07-10 16:58:27 +0900

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,2 @@
 DEST_DIR=/home/pi/kinokonow
-while read line; do
-  echo deploying $line
-  fab -H pi@192.168.0.14 copy_files:$line,${DEST_DIR}/$line
-done < .fabfiles
+fab -H pi@192.168.0.14 copy_files:.fabfiles,${DEST_DIR}
--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,10 @@
+import os
 from fabric.api import run, put
 
 
-def copy_files(local_path, remote_path):
-    print(local_path, remote_path)
+def copy_files(fab_files, remote_path):
+    files_to_deploy = open(fab_files).read().split()
+    print('deploying', files_to_deploy)
     run('mkdir -p %s' % remote_path)
-    put(local_path, remote_path)
+    for f in files_to_deploy:
+        put(f, os.path.join(remote_path, f))
