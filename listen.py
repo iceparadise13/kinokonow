240fc2aec4252b200991a599d300d025567c2942
k8tems <k8tems@live.com>
2017-07-13 14:54:47 +0900

    Localize db

--- listen.py
+++ listen.py
@@ -6,17 +6,13 @@ import mongo
 from util import load_yaml
 
 
-settings = load_yaml('settings.yml')
-db = mongo.connect(settings['mongo'])
-
-
 def convert_tweet_date(tweet_date):
     """Convert str datetime provided by twitter api to utc datetime object"""
     dt = datetime.strptime(tweet_date, '%a %b %d %H:%M:%S %z %Y')
     return (dt - dt.utcoffset()).replace(tzinfo=timezone.utc)
 
 
-def save_tweet(data):
+def save_tweet(db, data):
     # shallow copy suffices
     data = dict(data)
     data['created_at'] = convert_tweet_date(data['created_at'])
@@ -43,6 +39,9 @@ def get_followers():
 
 
 if __name__ == '__main__':
+    settings = load_yaml('settings.yml')
+    db = mongo.connect(settings['mongo'])
+
     yahoo_api_key = settings['yahoo_api_key']
     users_to_follow = get_followers()
     print('following %d users' % len(users_to_follow))
@@ -50,7 +49,7 @@ if __name__ == '__main__':
     def on_success(data):
         if 'text' in data:
             print(data['user']['screen_name'], ':', data['text'], '\n')
-            save_tweet(data)
+            save_tweet(db, data)
             tasks.create_noun_extraction_task(yahoo_api_key, data['text']).delay()
 
     stream = get_streamer(settings['twitter'])
