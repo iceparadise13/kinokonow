c89cf87c31d837f65506bee240b6e174921414c8
k8tems <k8tems@live.com>
2017-07-17 20:55:18 +0900

    Request nouns from server

--- ma.py
+++ ma.py
@@ -1,2 +1,7 @@
+import json
+import requests
+
+
 def extract_nouns_from_ma_server(tweet, host, port):
-    pass
+    resp = requests.get('http://%s:%d/parse?tweet=%s' % (host, port, tweet))
+    return json.loads(resp.content.decode('utf-8'))
