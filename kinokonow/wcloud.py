82cdc8c1c827897c078b40b2a38ed19040189620
k8tems <k8tems@live.com>
2017-08-03 14:32:47 +0900

    Rename method

--- kinokonow/wcloud.py
+++ kinokonow/wcloud.py
@@ -1,7 +1,7 @@
 import wordcloud
 
 
-def generate(frequencies, **kwargs):
+def generate_image(frequencies, **kwargs):
     # 値が0の要素があるとゼロ除算が起きるので事前に除く
     frequencies = dict([(k, v) for k, v in frequencies.items() if v])
     return wordcloud.WordCloud(**kwargs).generate_from_frequencies(frequencies).to_image()
