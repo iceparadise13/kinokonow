94f7284907c66069ccdab883e0fce5e6a53ab3d3
k8tems <k8tems@live.com>
2017-08-03 14:18:47 +0900

    Remove legacy method

--- kinokonow/words.py
+++ kinokonow/words.py
@@ -3,11 +3,5 @@ from operator import itemgetter
 import wordcloud
 
 
-def print_frequencies(frequencies):
-    frequencies = list(frequencies.items())
-    pprint.pprint(sorted(frequencies, key=itemgetter(1)))
-    print('%d nouns' % len(frequencies))
-
-
 def generate_word_cloud(frequencies, **kwargs):
     return wordcloud.WordCloud(**kwargs).generate_from_frequencies(frequencies).to_image()
